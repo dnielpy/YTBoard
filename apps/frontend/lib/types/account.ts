1976429daf9ac0a49101ec736536111944030e2a
dnielpy <dnielquesada@gmail.com>
2026-03-03 21:50:00 -0500

    feat(web): add account types

---
+++ apps/frontend/lib/types/account.ts
@@ -0,0 +1,49 @@
+export type AccountName = "Youtube"
+
+export type ViewsStats = {
+    total: number
+    perMonth: {
+        month: string
+        views: number
+    }[]
+    perVideo: {
+        videoID: string
+        views: number
+    }
+}
+
+export type FollowerStats = {
+    total: number
+    perMonth: {
+        month: string
+        views: number
+    }[]
+    perVideo: {
+        videoID: string
+        views: number
+    }
+}
+
+export type EarningStats = {
+    total: number
+    perMonth: {
+        month: string
+        views: number
+    }[]
+    perVideo: {
+        videoID: string
+        views: number
+    }
+}
+
+export type Stats = {
+    viewStats: ViewsStats
+    followerStat: FollowerStats
+    earningStat: EarningStats
+}
+
+export type Account = {
+    name: AccountName
+    userName: string
+    stats: Stats
+}
